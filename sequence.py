53594ce4cd9dea8a4d7069bb18e4241b2fb314b6
Bjarki Sig <bjarki@absalon.is>
2020-09-03 09:54:04 +0000

    added a skeleton

---
+++ sequence.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+#Create a code that adds the sequence together
+
+#have a sum that add them together
+
+#use the for loop to print the numbers, where n is the highest value
+    #have add here where the i and sum add toghether
