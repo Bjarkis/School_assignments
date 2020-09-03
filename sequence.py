70573bf64637079c518dc27b9752ea1c91f5b510
Bjarki Sig <bjarki@absalon.is>
2020-09-03 09:57:43 +0000

    Incorrect code

--- sequence.py
+++ sequence.py
@@ -6,3 +6,10 @@ n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
 #use the for loop to print the numbers, where n is the highest value
     #have add here where the i and sum add toghether
+
+sum_int = 0
+
+for i in range(1, n+1):
+    sum_int += i
+    print(sum_int)
+
